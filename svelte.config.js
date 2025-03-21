3403fa2d8576d3966a76aed013e92853b6c7e223
Linus Gubenis <linus.gubenis@gmail.com>
2025-03-21 20:37:13 +0100

    fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/icke.berlin' : ''
+			base: ''
 		},
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
